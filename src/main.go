925cdc174f8f00430068bbd78b484ab911547c61
szykol <szymonkolton@gmail.com>
2020-01-10 19:01:46 +0100

    Add example of communication with api and jwt

--- src/main.go
+++ src/main.go
@@ -4,20 +4,52 @@ import (
 	"io/ioutil"
 	"net/http"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"bytes"
+	"github.com/BurntSushi/toml"
 )
 
+type Config struct {
+	Serial string
+}
+
 func main() {
-	resp, err := http.Get("http://api:8000")
+	
+	content, _ := ioutil.ReadFile("/secrets.toml")
+	var config Config
+	if _, err := toml.Decode(string(content), &config); err != nil {
+		fmt.Println("error")
+		fmt.Println(err)
+	}
+	fmt.Println(config.Serial)
+	var SERIAL = []byte(config.Serial)
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"testing": "if",
+		"everything": "works",
+		"properly": true,
+	})
+
+	tokenString, err := token.SignedString(SERIAL)
 	if err != nil {
+		fmt.Println("err")
 		fmt.Println(err)
 	}
 
-	defer resp.Body.Close()
+	fmt.Println(tokenString)
 
-	body,err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
+	req, err := http.NewRequest("POST", "http://api:8000/jwt", bytes.NewBuffer([]byte(tokenString)))
+    req.Header.Set("Content-Type", "application/json")
 
-	fmt.Println(string(body))
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+
+    fmt.Println("response Status:", resp.Status)
+    fmt.Println("response Headers:", resp.Header)
+    body, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println("response Body:", string(body))
 }
